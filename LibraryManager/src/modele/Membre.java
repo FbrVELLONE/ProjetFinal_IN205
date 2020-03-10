e4cabe53518de7b7b1469544be74abaf2df226cd
Fabricio V <fabriciovellone@gmail.com>
2020-03-10 11:27:32 +0100

    att: class Membre.java

--- LibraryManager/src/modele/Membre.java
+++ LibraryManager/src/modele/Membre.java
@@ -10,11 +10,58 @@ public class Membre {
         PREMIUM,
         VIP
     }
-    public static int id;
-    private String nom, prenom, email, telephone, adresse;
+    private static int id = 0;
+    private String nom, preNom, email, telephone, adresse;
 
-    Membre(){
-        
+    public Membre(String nom, String preNom, String email, String telephone, String adresse){
+        this.nom = nom;
+        this.preNom = preNom;
+        this.email = email;
+        this.telephone = telephone;
+        this.adresse = adresse;
+        id++;
     }
     
+        //Getters
+    public int getId(){
+        return id;
+    }
+
+    public String getNom(){
+        return nom;
+    }
+
+    public String getPreNom(){
+        return preNom;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public String getTelephone(){
+        return telephone;
+    }
+    public String getAdresse(){
+        return adresse;
+    }
+
+        //setters
+    public void setNom(String nom){
+        this.nom = nom;
+    }
+
+    public void setPreNom(String preNom){
+        this.preNom = preNom;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public void setTelephone(String telephone){
+        this.telephone = telephone;
+    }
+    public void setAdresse(String adresse){
+        this.adresse = adresse;
+    }
+
 }
